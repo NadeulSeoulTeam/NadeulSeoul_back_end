7ca6e4f2e9531ee292034a230a05e40ce8e3a20a
= <dgh03207@gmail.com>
2022-01-21 17:04:02 +0900

    Update : Application.java추가

---
+++ src/main/java/com/alzal/nadeulseoulbackend/domain/Application.java
@@ -0,0 +1,13 @@
+package com.alzal.nadeulseoulbackend.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing  //jpa Auditing 활성화
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
