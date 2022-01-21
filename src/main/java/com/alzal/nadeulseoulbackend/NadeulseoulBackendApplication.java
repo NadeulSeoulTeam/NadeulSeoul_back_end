ab71e88ec5b740e7863c3e1d667fcaedd5155814
= <dgh03207@gmail.com>
2022-01-21 16:10:50 +0900

    Add : 초기 프로젝트 업로드

---
+++ src/main/java/com/alzal/nadeulseoulbackend/NadeulseoulBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alzal.nadeulseoulbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadeulseoulBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NadeulseoulBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alzal/nadeulseoulbackend/NadeulseoulBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alzal.nadeulseoulbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NadeulseoulBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
