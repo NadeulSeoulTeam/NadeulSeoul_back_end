fa2b65bb336fe709003ed714a48fca0b67d6d6b3
= <dgh03207@gmail.com>
2022-01-24 14:16:17 +0900

    Update : 초기 세팅

--- src/main/java/com/alzal/nadeulseoulbackend/domain/Application.java
+++ src/main/java/com/alzal/nadeulseoulbackend/domain/Application.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(Application.class, args);
     }
 }
