6c774a61d63f3da2ca220043e204196985e2e039
nesllewr <41321080+nesllewr@users.noreply.github.com>
2022-01-28 10:28:54 +0900

    Update : store관련 Dto 재업로드

--- src/main/java/com/alzal/nadeulseoulbackend/domain/stores/dto/StoreInfo.java
+++ src/main/java/com/alzal/nadeulseoulbackend/domain/stores/dto/StoreInfo.java
@@ -1,5 +1,6 @@
-package com.alzal.nadeulseoulbackend.domain.stores.entity;
+package com.alzal.nadeulseoulbackend.domain.stores.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,4 +22,16 @@ public class StoreInfo {
     private String lat;
     private String lng;
 
+    @Builder
+    public StoreInfo(Long sotreSeq, String placeName, String categoryName, String addressName,
+                     String placeUrl, String phone, String lat, String lng) {
+        this.sotreSeq = sotreSeq;
+        this.placeName = placeName;
+        this.categoryName = categoryName;
+        this.addressName = addressName;
+        this.placeUrl = placeUrl;
+        this.phone = phone;
+        this.lat = lat;
+        this.lng = lng;
+    }
 }
