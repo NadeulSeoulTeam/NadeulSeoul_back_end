b4b419f3c5d70f1dad77bf9c8e3378508bd06fdf
= <dgh03207@gmail.com>
2022-02-08 03:35:17 +0900

    Update : yml auto-configuration 설정 제거

--- src/main/java/com/alzal/nadeulseoulbackend/domain/users/controller/IndexController.java
+++ src/main/java/com/alzal/nadeulseoulbackend/domain/users/controller/IndexController.java
@@ -1,8 +1,12 @@
 package com.alzal.nadeulseoulbackend.domain.users.controller;
 
+import com.alzal.nadeulseoulbackend.global.common.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class IndexController {
+
 
 }
