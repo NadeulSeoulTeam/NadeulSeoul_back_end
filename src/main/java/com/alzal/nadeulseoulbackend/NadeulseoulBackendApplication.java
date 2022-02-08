66db0409065f91f2980610d165a4391b0be5e034
nesllewr <41321080+nesllewr@users.noreply.github.com>
2022-02-08 20:49:53 +0900

    Add : AppProperties 추가

--- src/main/java/com/alzal/nadeulseoulbackend/NadeulseoulBackendApplication.java
+++ src/main/java/com/alzal/nadeulseoulbackend/NadeulseoulBackendApplication.java
@@ -1,8 +1,11 @@
 package com.alzal.nadeulseoulbackend;
 
+import com.alzal.nadeulseoulbackend.global.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(value = {AppProperties.class})
 @SpringBootApplication
 public class NadeulseoulBackendApplication {
     public static void main(String[] args) {
--- src/main/java/com/alzal/nadeulseoulbackend/global/config/SecurityConfig.java
+++ src/main/java/com/alzal/nadeulseoulbackend/global/config/SecurityConfig.java
@@ -1,33 +1,56 @@
 package com.alzal.nadeulseoulbackend.global.config;
 
+import com.alzal.nadeulseoulbackend.global.config.auth.HttpCookieOAuth2AuthorizationRequestRepository;
+import com.alzal.nadeulseoulbackend.global.config.auth.OAuth2AuthenticationSuccessHandler;
 import com.alzal.nadeulseoulbackend.global.config.auth.service.CustomOAuth2UserService;
 import com.alzal.nadeulseoulbackend.domain.users.entity.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
+@Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     private final CustomOAuth2UserService customOauth2UserService;
+
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+
+    @Bean
+    public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository(){
+        return new HttpCookieOAuth2AuthorizationRequestRepository();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().headers().frameOptions().disable()
+        http
+                .cors() //cors 허용
                 .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //Session 비활성화
+                .and()
+                .csrf().disable() //csrf 비활성화
+                .formLogin().disable() //로그인폼 비활성화
+                .httpBasic().disable() //기본 로그인 창 비활성화
                 .authorizeRequests()
                 .antMatchers("/api/v1/**").permitAll()
-                .antMatchers("/api/v1/profile/**").hasRole(Role.MEMBER.name()).anyRequest().authenticated()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
+                .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
-                .loginPage("/member/signin")
+                .authorizationEndpoint()
+                .baseUri("/oauth2/authorization") //클라이언트 첫 로그인 URI
+                .authorizationRequestRepository(cookieAuthorizationRequestRepository())
+                .and()
                 .userInfoEndpoint()
-                .userService(customOauth2UserService);
-//                .and()
-//                .failureHandler(oAuth2AuthenticationFailureHandler)
+                .userService(customOauth2UserService)
+                .and()
+                .successHandler(oAuth2AuthenticationSuccessHandler);
 
     }
 }
+
