6a6c48d2b7c6e6d1fd5869544ff6b532086af0a8
= <dgh03207@gmail.com>
2022-02-12 01:13:24 +0900

    Delete : 필요없는 코드 제거

--- src/main/java/com/alzal/nadeulseoulbackend/domain/users/service/UserInfoService.java
+++ src/main/java/com/alzal/nadeulseoulbackend/domain/users/service/UserInfoService.java
@@ -3,14 +3,11 @@ package com.alzal.nadeulseoulbackend.domain.users.service;
 import com.alzal.nadeulseoulbackend.domain.users.dto.SignupInfo;
 import com.alzal.nadeulseoulbackend.domain.users.entity.User;
 import com.alzal.nadeulseoulbackend.domain.users.repository.UserRepository;
-import com.alzal.nadeulseoulbackend.global.security.TokenProvider;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -18,8 +15,6 @@ public class UserInfoService {
 
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private TokenProvider tokenProvider;
 
     //User정보 등록하기
     @Transactional
